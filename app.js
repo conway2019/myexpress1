6fa69e88e17c156fea3bd6f875a4d05b7d7e8b8d
conway <kangwei@xinhua.org>
2020-01-08 09:53:25 +0800

    my express 1

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var booksRouter = require('./routes/books');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/books', booksRouter);
+
+module.exports = app;
---
+++ routes/books.js
@@ -0,0 +1,40 @@
+/*
+const fs = require("fs");
+let inputStream = fs.createReadStream('./public/books.json');
+
+inputStream.on('data',function (chunk) {
+    books = chunk.toString();
+})
+inputStream.on('end',function () {
+    inputStream.close()
+    console.log("books初始化完成 ")
+})
+*/
+
+var express = require('express');
+var router = express.Router();
+
+var maxId = 2;
+var books = [{_id: 1, name: "book1", price: 100},
+    {_id: 2, name: "book2", price: 200}];
+
+router.route('/')
+    .get(function (req, res) {
+        res.json(books)
+    })
+    .post(function (req, res) {
+        let book = req.body;
+        book._id = ++maxId;
+        books.push(book);
+        res.json(book);
+    })
+
+router.delete('/:id', function (req, res) {
+    //let id = req.param("id");
+    let id = req.params.id;
+    let index = books.findIndex(book => book._id == id);
+    books.splice(index, 1);
+    res.json({})
+})
+
+module.exports = router;
